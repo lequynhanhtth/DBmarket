523ac216f54af31598ef72ace48c7328346bffaa
lequynhanhtth <lequynhanhtth@gmail.com>
2021-12-19 22:40:24 +0700

    nhan 19/12

--- src/main/java/com/example/dbmarket/controller/CategoryController.java
+++ src/main/java/com/example/dbmarket/controller/CategoryController.java
@@ -16,9 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -49,36 +47,41 @@ public class CategoryController {
         return "views/content/category";
     }
 
-    @PostMapping("/Category/filter")
-    public String filterData(int categoryId, int[] categoryProducts, int[] brands, Optional<Long> minPrice, Optional<Long> maxPrice, Optional<Integer> page,Model model) {
+    @GetMapping("/Category/filter")
+    public String filterData(int categoryId, int[] categoryProducts, int[] brands, Optional<Long> minPrice, Optional<Long> maxPrice, Optional<Integer> page, Model model, Optional<String> search) {
         Pageable pageable = PageRequest.of(page.orElse(0), 12);
         List<CategoryProduct> categoryProductList = categoryProductService.findAll();
         List<Brand> brandList = brandService.findAll();
         List<Integer> b = new ArrayList<>();
         List<Integer> c = new ArrayList<>();
-        if(categoryProducts == null){
-            for(CategoryProduct categoryProduct : categoryProductList ){
+        if (categoryProducts == null) {
+            for (CategoryProduct categoryProduct : categoryProductList) {
                 c.add(categoryId);
             }
-        }else{
-            for(int categoryProduct : categoryProducts){
+        } else {
+            for (int categoryProduct : categoryProducts) {
                 c.add(categoryProduct);
             }
         }
-        if(brands == null){
-            for(Brand brand : brandList){
+        if (brands == null) {
+            for (Brand brand : brandList) {
                 b.add(brand.getBrandId());
             }
-        }else {
-            for(int brand : brands){
+        } else {
+            for (int brand : brands) {
                 b.add(brand);
             }
         }
-        Page<Product> productPage = productService.findManyOption(b, c, minPrice.orElse((long)0), maxPrice.orElse((long)10000000), categoryId, pageable);
-        model.addAttribute("productPage",productPage);
+        Page<Product> productPage = productService.findManyOption(b, c, minPrice.orElse((long) 0), maxPrice.orElse((long) 10000000), categoryId, pageable,"%"+search.orElse("") +"%");
+        model.addAttribute("productPage", productPage);
         model.addAttribute("brands", brandList);
         model.addAttribute("categoryProducts", categoryProductList);
         model.addAttribute("categoryId", categoryId);
+        model.addAttribute("categoryProductsArray", c);
+        model.addAttribute("brandsArray", b);
+        model.addAttribute("minPrice", minPrice);
+        model.addAttribute("maxPrice", maxPrice);
+        model.addAttribute("search");
         return "views/content/category";
     }
 
--- src/main/java/com/example/dbmarket/service/ProductService.java
+++ src/main/java/com/example/dbmarket/service/ProductService.java
@@ -26,7 +26,7 @@ public interface ProductService {
 
     Page<Product> findByCategoryId(int id, Pageable pageable);
 
-    Page<Product> findManyOption(List<Integer> brands, List<Integer> categoryProducts, long minPrice, long maxPrice, int categoryId, Pageable pageable);
+    Page<Product> findManyOption(List<Integer> brands, List<Integer> categoryProducts, long minPrice, long maxPrice, int categoryId, Pageable pageable,String productName);
 
     Page<Product> findAll(Pageable pageable);
 }
