3ce458346835127f3398a4a63e0275b64c606a26
lequynhanhtth <lequynhanhtth@gmail.com>
2021-12-21 05:05:27 +0700

    nhan 21/12

--- src/main/java/com/example/dbmarket/repository/OrderDetailRepository.java
+++ src/main/java/com/example/dbmarket/repository/OrderDetailRepository.java
@@ -4,6 +4,11 @@ import com.example.dbmarket.entities.Category;
 import com.example.dbmarket.entities.Order;
 import com.example.dbmarket.entities.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail,Integer> {
+    @Query(value = "SELECT SUM(o.quantity) FROM OrderDetail o WHERE o.order.status like 'Done' and o.order.supplier.supplierId = :supplierId")
+    Double getNumberProductBySupplierId(int supplierId);
+    @Query(value = "SELECT SUM(o.quantity*o.product.price) FROM OrderDetail o WHERE o.order.status like 'Done' and o.order.supplier.supplierId = :supplierId")
+    Double getTotalPriceBySupplierId(int supplierId);
 }
