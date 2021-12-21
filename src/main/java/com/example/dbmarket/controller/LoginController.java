f80fb42e36db16752e427b4e57188ecc38f016de
lequynhanhtth <lequynhanhtth@gmail.com>
2021-12-21 10:26:16 +0700

    nhan 21/12

--- src/main/java/com/example/dbmarket/controller/LoginController.java
+++ src/main/java/com/example/dbmarket/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -20,10 +21,12 @@ public class LoginController {
     HttpSession session;
     @Autowired
     SupplierService supplierService;
+
     @GetMapping("login")
     public String showLogin() {
         return "views/content/login";
     }
+
     @GetMapping("doLogin")
 
     public String doLogin(Model model, String email, String password, String isUser) {
@@ -32,51 +35,77 @@ public class LoginController {
             Customer customer = customerService.findByEmail(email).orElse(null);
             if (customer != null) {
                 if (password.equals(customer.getPassword())) {
-                    session.setAttribute("customer",customer);
+                    session.setAttribute("customer", customer);
                     return "redirect:/DBmarket/Home";
-                }else{
+                } else {
                     model.addAttribute("messageLogin", "Sai mật khẩu");
                     return "views/content/login";
                 }
 
-            }else{
+            } else {
                 model.addAttribute("messageLogin", "Tài khoản không tồn tại ");
                 return "views/content/login";
             }
 
-        }else{
+        } else {
             Supplier supplier = supplierService.findByEmail(email).orElse(null);
-            if(supplier != null){
-                if(password.equals(supplier.getPassword())){
-                    session.setAttribute("supplier",supplier);
+            if (supplier != null) {
+                if (password.equals(supplier.getPassword())) {
+                    session.setAttribute("supplier", supplier);
                     return "redirect:/supplier/dashboard";
-                }else{
+                } else {
                     model.addAttribute("messageLogin", "Sai mật khẩu");
                     return "views/content/login";
                 }
 
-            }else{
+            } else {
                 model.addAttribute("messageLogin", "Tài khoản không tồn tại ");
                 return "views/content/login";
             }
         }
     }
+
     @RequestMapping("/customer/logout")
-    public String logout(){
+    public String logout() {
         session.removeAttribute("customer");
         return "redirect:/DBmarket/Home";
     }
 
     @RequestMapping("/supplier/logout")
-    public String logoutSupplier(){
+    public String logoutSupplier() {
         session.removeAttribute("supplier");
         return "redirect:/DBmarket/Home";
     }
 
     @RequestMapping("/admin/logout")
-    public String logoutAdmin(){
+    public String logoutAdmin() {
         session.removeAttribute("admin");
         return "redirect:/DBmarket/Home";
     }
 
+    @GetMapping("/register")
+    public String register() {
+        return "views/content/register";
+    }
+
+    @PostMapping("/register")
+    public String doRegister(String username, String email, String password, String isUser) {
+        if (isUser.equals("User")) {
+            Customer customer = new Customer();
+            customer.setPassword(password);
+            customer.setUsername(username);
+            customer.setEmail(email);
+            customer.setStatus(true);
+            customerService.save(customer);
+        } else {
+            Supplier supplier = new Supplier();
+            supplier.setEmail(email);
+            supplier.setPassword(password);
+            supplier.setUsername(username);
+            supplier.setEmail(email);
+            supplier.setStatus(true);
+            supplierService.save(supplier);
+        }
+        return "redirect:/login";
+    }
 }
